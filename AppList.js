655d5bb70bfcd75b912af70e173f5c1a5923bce9
Fabio Matsunaga <fabio.matsunaga@sesisenaipr.org.br>
2021-10-29 19:16:38 -0300

    estilos definidos

--- AppList.js
+++ AppList.js
@@ -13,9 +13,29 @@ export default function AppList() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#D93600',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#D93600',
+      alignItems: 'center',
+      justifyContent: 'center'
     },
-});
+    title: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 50,
+      marginBottom: 20
+    },
+    scrollContainer: {
+      flex: 1,
+      width: '90%'
+    },
+    itemsContainer: {
+      flex: 1,
+      marginTop: 10,
+      padding: 20,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      alignItems: 'stretch',
+      backgroundColor: '#fff'
+    },
+  });
