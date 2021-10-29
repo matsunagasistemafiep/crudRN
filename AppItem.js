9e8e4e64c258c5ba322960d0f6fefcbcb288850e
Fabio Matsunaga <fabio.matsunaga@sesisenaipr.org.br>
2021-10-29 20:56:30 -0300

    novos botões

--- AppItem.js
+++ AppItem.js
@@ -5,6 +5,14 @@ export default function AppItem(props) {
     return (
         <View style={styles.container}>
           <Text style={styles.textItem}> {props.item} </Text>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.deleteButton} > 
+                <Text style={styles.buttonText}>X</Text> 
+            </TouchableOpacity> 
+            <TouchableOpacity style={styles.editButton} > 
+                <Text style={styles.buttonText}>Editar</Text> 
+            </TouchableOpacity> 
+          </View>
         </View>
       );
 }
